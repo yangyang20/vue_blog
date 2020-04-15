bf6de1fd4bc7adececa307d64c695fafe7941e63
yangyang20 <393622951@qq.com>
2020-04-15 20:24:34 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,67 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from "../views/home/Home";
+import Articles from "../views/articles/Articles";
+import Timeline from "../views/timeline/Timeline";
+import Project from "../views/project/Project";
+import About from "../views/about/About";
+import Tattle from "../views/tattle/Tattle";
+import TattleDetail from "../views/tattle/TattleDetail";
+import ArticlesDetail from "../views/articles/childComps/ArticlesDetail";
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect:'home'
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
+    path: '/articles',
+    name: 'Articles',
+    component: Articles
+  },
+  {
+    path: '/timeline',
+    name: 'Timeline',
+    component: Timeline
+  },
+  {
+    path: '/project',
+    name: 'Project',
+    component: Project
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path: '/tattle',
+    name: 'Tattle',
+    component: Tattle,
+
+  },
+  {
+    path:'/tattleDetail',
+    name:'TattleDetail',
+    component:TattleDetail,
+  },
+  {
+    path:'/articlesDetail',
+    name:'ArticlesDetail',
+    component:ArticlesDetail,
   }
 ]
 
 const router = new VueRouter({
+  mode:'history',
   routes
 })
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import pages from "./modules/pages";
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+const store = new Vuex.Store({
+  modules:{
+    pages
   }
 })
+export default store
+
